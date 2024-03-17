c7b0c4428fe8a7dca381028238d48b73a0617b24
Alexey Seren <dehaekx@gmail.com>
2024-03-17 17:48:31 +0700

    i added new comments for classes, widgets, constructors and tested some function by changed them

--- win.cpp
+++ win.cpp
@@ -1,63 +1,65 @@
 #include "win.h"
 
-Win::Win(QWidget *parent):QWidget(parent) // конструктор
+Win::Win(QWidget *parent): QWidget(parent) // конструктор, который создает объеты и строит пользовательское окно
 {
-    //codec = QTextCodec::codecForName("Windows-1251");
-    setWindowTitle(("Возведение в квадрат"));
-    frame = new QFrame(this);
-    frame->setFrameShadow(QFrame::Raised);
-    frame->setFrameShape(QFrame::Panel);
-    inputLabel = new QLabel(("Введите число:"),
-                            this);
-    inputEdit = new QLineEdit("",this);
-    StrValidator *v=new StrValidator(inputEdit);
+    setWindowTitle(("Возведение в квадрат")); // Установливаем заголовок окна
+    frame = new QFrame(this); // Создаем объекта QFrame
+    frame->setFrameShadow(QFrame::Raised); // Установливаем стиль тени для рамки объекта QFrame
+    frame->setFrameShape(QFrame::Panel); // Установливаем форму рамки объекта QFrame
+
+    inputLabel = new QLabel(("Введите число:"), this); // Создаем объект QLabel с текстом "Введите число"
+    inputEdit = new QLineEdit("", this); // Создаем объект QLineEdit для ввода текста
+    StrValidator *v = new StrValidator(inputEdit); // Создаем объект валидатора для проверки вводимых данных
     inputEdit->setValidator(v);
-    outputLabel = new QLabel(("Результат:"),
-                             this);
-    outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton(("Следующее"),
-                                 this);
-    exitButton = new QPushButton(("Выход"),
-                                 this);
+    outputLabel = new QLabel(("Результат:"), this); // Созданем объект QLabel с текстом "Результат"
+    outputEdit = new QLineEdit("", this); // Создаем объект QLineEdit для отображения результата вычислений
+    nextButton = new QPushButton(("Следующее"), this); // Создаем кнопки "Следующее"
+    exitButton = new QPushButton(("Выход"), this); // создаем кнопку выход
 
     // компоновка приложения выполняется согласно рисунку 2.
-    QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
-    vLayout1->addWidget(inputLabel);
+    QVBoxLayout *vLayout1 = new QVBoxLayout(frame); // Создаем вертикальную компоновку для объекта frame
+    vLayout1->addWidget(inputLabel); // Добавляем объект inputLabel в компоновку vLayout1
     vLayout1->addWidget(inputEdit);
     vLayout1->addWidget(outputLabel);
     vLayout1->addWidget(outputEdit);
-    vLayout1->addStretch();
-    QVBoxLayout *vLayout2 = new QVBoxLayout();
-    vLayout2->addWidget(nextButton);
-    vLayout2->addWidget(exitButton);
-    vLayout2->addStretch();
-    QHBoxLayout *hLayout = new QHBoxLayout(this);
-    hLayout->addWidget(frame);
-    hLayout->addLayout(vLayout2);
+    vLayout1->addStretch(); // Добавляем упругий интервал в компоновку vLayout1
+
+    QVBoxLayout *vLayout2 = new QVBoxLayout(); // Создаем вертикальную компоновку vLayout2
+    vLayout2->addWidget(nextButton); // Добавление кнопки nextButton в компоновку vLayout2
+    vLayout2->addWidget(exitButton); // Добавление кнопки exitButton в компоновку vLayout2
+    vLayout2->addStretch(); // Добавляем упругий интервал в компоновку vLayout2
+
+    QHBoxLayout *hLayout = new QHBoxLayout(this); // Создание горизонтальной компоновки hLayout
+    hLayout->addWidget(frame); // Добавление объекта frame в компоновку hLayout
+    hLayout->addLayout(vLayout2); // Добавление компоновки vLayout2 в hLayout
     begin();
     // connect(exitButton,SIGNAL(clicked(bool)),
     //         this,SLOT(close()));
+    // Установка соединения между событием "клик по кнопке exitButton" и методом close()
     connect(exitButton, &QPushButton::clicked, this, &Win::close);
 
     // connect(nextButton,SIGNAL(clicked(bool)),
     //         this,SLOT(begin()));
+    // Установка соединения между событием "клик по кнопке nextButton" и методом begin()
     connect(nextButton, &QPushButton::clicked, this, &Win::begin);
 
     // connect(inputEdit,SIGNAL(returnPressed()),
     //         this,SLOT(calc()));
+    // Установка соединения между событием "ввод текста и нажатие Enter в inputEdit" и методом calc()
     connect(inputEdit, &QLineEdit::returnPressed, this, &Win::calc);
 }
 void Win::begin()
 {
-    inputEdit->clear();
-    nextButton->setEnabled(false);
-    nextButton->setDefault(false);
-    inputEdit->setEnabled(true);
-    outputLabel->setVisible(false);
-    outputEdit->setVisible(false);
-    outputEdit->setEnabled(false);
-    inputEdit->setFocus();
+    inputEdit->clear(); // очищает поле ввода
+    nextButton->setEnabled(false); // недоступность nextButton
+    nextButton->setDefault(false); // не выделяется кнопка nextButton изначально
+    inputEdit->setEnabled(true); // поле ввода доступно
+    outputLabel->setVisible(false); // label  "результат" не виден
+    outputEdit->setVisible(false); // поле результата вычисления не виде
+    outputEdit->setEnabled(false); // не дает редактировать поле с результатом
+    inputEdit->setFocus(); // выделяется поле ввода после нажатия nextButton
 }
+
 void Win::calc()
 {
     bool Ok=true;
--- win.h
+++ win.h
@@ -1,24 +1,26 @@
-#ifndef win_h
+#ifndef win_h   // Директивы препроцессора для предотвращения многократного включения заголовочного файла
 #define win_h
-#include <QtGui>
 
-#include <QtWidgets>
+#include <QtWidgets>    // подключение библиотеки для всех объектов пользовательского интерфейса
 
-class Win: public QWidget // класс окна
+class Win: public QWidget // класс окна, представляет пользовательское окно
 {
     Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+
 protected:
-    //QTextCodec *codec;
+    // Объявление указателей на различные виджеты, которые будут использоваться в пользовательском интерфейсе окна
     QFrame *frame; // рамка
     QLabel *inputLabel; // метка ввода
-    QLineEdit *inputEdit; // строчный редактор ввода
+    QLineEdit *inputEdit; // строчный редактор ввода - куда вводим число
     QLabel *outputLabel; // метка вывода
     QLineEdit *outputEdit; // строчный редактор вывода
-    QPushButton *nextButton; // кнопка Следующее
-    QPushButton *exitButton; // кнопка Выход
+    QPushButton *nextButton; // кнопка Следующее - следующий пример
+    QPushButton *exitButton; // кнопка Выход - завершение работы приложения
+
 public:
-    Win(QWidget *parent = 0); // конструктор
+    Win(QWidget *parent = 0); // конструктор класса, родитель по умолчанию 0
 public slots:
+    // методы класса
     void begin(); // метод начальной настройки интерфейса
     void calc(); // метод реализации вычислений
 };
