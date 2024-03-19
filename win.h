2d5ad8ff3b35f354b80dbcf91d838415ed7e5716
Alexey Seren <dehaekx@gmail.com>
2024-03-19 17:07:22 +0700

    add code from book and fix places with is not correct code

--- win.h
+++ win.h
@@ -1,34 +1,37 @@
 #ifndef win_h
 #define win_h
-#include <QtGui>
 
-#include <QWidget>
+#include <QtWidgets>
 
-class Win:public QWidget // класс окна
+class Counter:public QLineEdit
 {
-    Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+    Q_OBJECT
+public:
+    Counter(const QString & contents, QWidget *parent=0):
+        QLineEdit(contents,parent){}
+signals:
+    void tick_signal();
+public slots:
+    void add_one()
+    {
+        QString str=text();
+        int r=str.toInt();
+        if (r!=0 && r%5 ==0) emit tick_signal();
+        r++;
+        str.setNum(r);
+        setText(str);
+    }
+};
+class Win: public QWidget
+{
+    Q_OBJECT
 protected:
     QTextCodec *codec;
-    QFrame *frame; // рамка
-    QLabel *inputLabel; // метка ввода
-    QLineEdit *inputEdit; // строчный редактор ввода
-    QLabel *outputLabel; // метка вывода
-    QLineEdit *outputEdit; // строчный редактор вывода
-    QPushButton *nextButton; // кнопка Следующее
-    QPushButton *exitButton; // кнопка Выход
+    QLabel *label1,*label2;
+    Counter *edit1,*edit2;
+    QPushButton *calcbutton;
+    QPushButton *exitbutton;
 public:
-    Win(QWidget *parent = 0); // конструктор
-public slots:
-    void begin(); // метод начальной настройки интерфейса
-    void calc(); // метод реализации вычислений
-};
-class StrValidator:public QValidator // класс компонента проверки ввода
-{
-public:
-    StrValidator(QObject *parent):QValidator(parent){}
-    virtual State validate(QString &str,int &pos)const
-    {
-        return Acceptable; // метод всегда принимает вводимую строку
-    }
+    Win(QWidget *parent = 0);
 };
 #endif
