7146a5ad0054f98a1ea7da00d620749c9b7ea5f9
Alexey Seren <dehaekx@gmail.com>
2024-03-22 19:42:44 +0700

    add "override" in win.h for virtual function validate

--- win.h
+++ win.h
@@ -29,7 +29,7 @@ class StrValidator: public QValidator // класс компонента про�
 {
 public:
     StrValidator(QObject *parent):QValidator(parent){}
-    virtual State validate(QString &str,int &pos)const // Зачем метод и const
+    State validate(QString &str,int &pos)const override // Зачем метод и const
     {
         auto msg = QMessageBox(
             QMessageBox::Information,
